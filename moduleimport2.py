00f0eb6f27c4b88d35c65c15f687818257feb332
DevprabhaTomar <devprabhatomar@gmail.com>
2026-02-19 21:59:44 -0800

    Add files via upload

---
+++ moduleimport2.py
@@ -0,0 +1,9 @@
+#to import the module
+from import numericcalculation import *
+
+#to display addition of two numbers
+displayaddition(10,5)
+#to display difference of two numbers
+displaydifference(25,45)
+#to display product of two numbers
+display_product(15,20)
