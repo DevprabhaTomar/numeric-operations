1494d699eebf5673facae609eaf38501d0d8e0fc
DevprabhaTomar <devprabhatomar@gmail.com>
2026-02-19 22:00:06 -0800

    Add files via upload

---
+++ oddeven.py
@@ -0,0 +1,6 @@
+from numericcalculation import calculateremainder
+num = int(input("Enter a number:"))
+if(calculatoremainder(num,2) == 0):
+    print(num, " is an even number .")
+else:
+    print(num " is an odd number .")
